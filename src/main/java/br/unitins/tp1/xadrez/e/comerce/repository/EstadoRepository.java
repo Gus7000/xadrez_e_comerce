bb28ef563add61c972897dfb51180ce5e4ff873e
Gustavo <gustavosilva17@unitins.br>
2026-03-02 21:36:44 -0300

    inicio do projeto de e-comerce de xadrez

---
+++ src/main/java/br/unitins/tp1/xadrez/e/comerce/repository/EstadoRepository.java
@@ -0,0 +1,12 @@
+package br.unitins.tp1.xadrez.e.comerce.repository;
+
+import br.unitins.tp1.xadrez.e.comerce.model.Estado;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class EstadoRepository implements PanacheRepository<Estado>{
+    
+
+
+}
