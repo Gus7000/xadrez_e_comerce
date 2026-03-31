7bc111b8c79a2f63f077c4ba63698894b5471f25
Gustavo <gustavosilva17@unitins.br>
2026-03-31 20:53:48 -0300

    arrumnei o nome de RelogioService

---
+++ src/main/java/br/unitins/tp1/xadrez/e/comerce/service/RelogioService.java
@@ -0,0 +1,21 @@
+package br.unitins.tp1.xadrez.e.comerce.service;
+
+import java.util.List;
+
+import br.unitins.tp1.xadrez.e.comerce.model.Relogio;
+
+public interface RelogioService {
+    List<Relogio> findAll();
+
+    Relogio findById(Long id);
+
+    List<Relogio> findByMarca(String marca);
+
+    List <Relogio> findByTipo(Long idTipo);
+
+    Relogio create(Relogio relogio);
+
+    void update(Long id, Relogio relogio);
+
+    void delete(Long id);
+}
